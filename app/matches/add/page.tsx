c730538d0863c49df70ae9b6866511c30ee49835
alexandrtovmach <alexandrtovmach@gmail.com>
2024-06-21 16:10:58 +0200

    feat: Added match page

---
+++ app/matches/add/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Container } from "@chakra-ui/react";
+
+const AddMatch: React.FC = () => {
+  return (
+    <Container>
+      Add new match
+    </Container>
+  );
+}
+
+export default AddMatch;
