192775b6f76b281b8bc031a645ece1e072970794
alexandrtovmach <alexandrtovmach@gmail.com>
2024-06-21 18:50:06 +0200

    feat: Updated migrations and schema

--- prisma/migrations/20240621155143_initialize/migration.sql
+++ prisma/migrations/20240621155143_initialize/migration.sql
@@ -1,13 +1,14 @@
 -- CreateTable
 CREATE TABLE "archives" (
     "id" TEXT NOT NULL,
+    "code" VARCHAR(10) NOT NULL,
     "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(6),
     "title" VARCHAR(255),
     "info" VARCHAR(255),
     "url" VARCHAR(255),
     "address" VARCHAR(255),
-    "phone_number" VARCHAR(50),
+    "phone_number" VARCHAR(20),
     "email" VARCHAR(255),
     "logo_url" VARCHAR(255),
 
@@ -17,6 +18,7 @@ CREATE TABLE "archives" (
 -- CreateTable
 CREATE TABLE "funds" (
     "id" TEXT NOT NULL,
+    "code" VARCHAR(10) NOT NULL,
     "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(6),
     "title" VARCHAR(255),
@@ -29,6 +31,7 @@ CREATE TABLE "funds" (
 -- CreateTable
 CREATE TABLE "descriptions" (
     "id" TEXT NOT NULL,
+    "code" VARCHAR(10) NOT NULL,
     "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(6),
     "title" VARCHAR(255),
@@ -41,6 +44,7 @@ CREATE TABLE "descriptions" (
 -- CreateTable
 CREATE TABLE "cases" (
     "id" TEXT NOT NULL,
+    "code" VARCHAR(20) NOT NULL,
     "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(6),
     "title" VARCHAR(255),
@@ -60,7 +64,6 @@ CREATE TABLE "resources" (
     "info" VARCHAR(255),
     "url" VARCHAR(255),
     "logo_url" VARCHAR(255),
-    "table_name" VARCHAR(50) NOT NULL,
 
     CONSTRAINT "resources_pkey" PRIMARY KEY ("id")
 );
@@ -74,6 +77,7 @@ CREATE TABLE "matches" (
     "resource_id" VARCHAR(50) NOT NULL,
     "api_criteria" VARCHAR(255) NOT NULL,
     "api_url" VARCHAR(255) NOT NULL,
+    "archive_id" VARCHAR(50),
     "fund_id" VARCHAR(50),
     "description_id" VARCHAR(50),
     "case_id" VARCHAR(50),
@@ -92,6 +96,15 @@ CREATE TABLE "results" (
     CONSTRAINT "results_pkey" PRIMARY KEY ("match_id","created_at")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "funds_code_archive_id_key" ON "funds"("code", "archive_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "descriptions_code_fund_id_key" ON "descriptions"("code", "fund_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "cases_code_description_id_key" ON "cases"("code", "description_id");
+
 -- AddForeignKey
 ALTER TABLE "funds" ADD CONSTRAINT "funds_archive_id_fkey" FOREIGN KEY ("archive_id") REFERENCES "archives"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
@@ -103,6 +116,9 @@ ALTER TABLE "cases" ADD CONSTRAINT "cases_description_id_fkey" FOREIGN KEY ("des
 
 -- AddForeignKey
 ALTER TABLE "matches" ADD CONSTRAINT "matches_resource_id_fkey" FOREIGN KEY ("resource_id") REFERENCES "resources"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "matches" ADD CONSTRAINT "matches_archive_id_fkey" FOREIGN KEY ("archive_id") REFERENCES "archives"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE "matches" ADD CONSTRAINT "matches_fund_id_fkey" FOREIGN KEY ("fund_id") REFERENCES "funds"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
