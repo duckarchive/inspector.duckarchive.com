c9f5d95a813173cf7569b399c70621e1874a7ef1
alexandrtovmach <alexandrtovmach@gmail.com>
2024-06-24 14:05:28 +0200

    feat: Added diff calc for archium

--- pages/api/sync/archium/index.ts
+++ pages/api/sync/archium/index.ts
@@ -1,31 +1,38 @@
-import { Archive, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { parse } from "node-html-parser";
 import axios from "axios";
 import { parseDBParams } from "../../helpers";
 
+const ARCHIUM_RESOURCES = [
+  "5abef7c0-6b5b-43a5-9271-232b1e24266a",
+  "3cc6ab5e-6bc3-4775-808b-d7e918687ec5",
+  "58c8d14e-36a6-4ea4-8429-92688451d223",
+  "d07f3552-19b7-4aaf-bb7a-c10888f6a95f",
+  "c8a60530-c59c-4ef1-8860-7a364c462d26",
+  "8b2e5d9a-4369-47c0-bb2b-eb2e8252009d",
+  "ac4d6a76-4cdc-4382-ba7d-aeb42e9ef737",
+  "b73cfbed-3dde-439f-9f7b-acb0e4b31b70",
+  "4dca72d1-7be6-40d0-b1ae-9d51d1e976fe",
+];
+
+interface SyncArchiumResponse {
+  match_id: string;
+  total: number;
+  diff: number;
+}
+
 const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<(Archive & { total: number })[]>
+  res: NextApiResponse<SyncArchiumResponse[]>
 ) {
-  // READ ALL DATA
   if (req.method === "GET") {
     const archiumMatches = await prisma.match.findMany({
       where: {
         resource_id: {
-          in: [
-            "5abef7c0-6b5b-43a5-9271-232b1e24266a",
-            "3cc6ab5e-6bc3-4775-808b-d7e918687ec5",
-            "58c8d14e-36a6-4ea4-8429-92688451d223",
-            "d07f3552-19b7-4aaf-bb7a-c10888f6a95f",
-            "c8a60530-c59c-4ef1-8860-7a364c462d26",
-            "8b2e5d9a-4369-47c0-bb2b-eb2e8252009d",
-            "ac4d6a76-4cdc-4382-ba7d-aeb42e9ef737",
-            "b73cfbed-3dde-439f-9f7b-acb0e4b31b70",
-            "4dca72d1-7be6-40d0-b1ae-9d51d1e976fe",
-          ],
+          in: ARCHIUM_RESOURCES,
         },
         archive_id: {
           not: null,
@@ -36,12 +43,9 @@ export default async function handler(
       },
     });
 
-    const archivesWithTotals = await Promise.all(
+    const results: SyncArchiumResponse[] = await Promise.all(
       archiumMatches.map(
-        async ({ archive, api_url, api_method, api_headers, api_params }) => {
-          if (!archive) {
-            throw new Error("Archive not found");
-          }
+        async ({ id, api_url, api_method, api_headers, api_params }) => {
           try {
             const {
               data: { View },
@@ -62,22 +66,51 @@ export default async function handler(
               .filter(Boolean)
               .reduce((prev, el) => (prev += el), 0);
 
+            const prevResult = await prisma.result.findFirst({
+              where: {
+                match_id: id,
+                error: null,
+              },
+              orderBy: {
+                created_at: "desc",
+              },
+            });
+
+            await prisma.result.create({
+              data: {
+                match_id: id,
+                count: totalCases,
+                error: null,
+              },
+            });
+
+            if (prevResult) {
+              return {
+                match_id: id,
+                total: totalCases,
+                diff: totalCases - prevResult.count,
+              };
+
+            }
+
             return {
-              ...archive,
+              match_id: id,
               total: totalCases,
+              diff: 0,
             };
-          } catch (error) {
+          } catch (error: any) {
             console.error(error);
             return {
-              ...archive,
+              match_id: id,
               total: 0,
+              diff: 0,
             };
           }
         }
       )
     );
 
-    res.json(archivesWithTotals);
+    res.json(results);
   } else {
     res.status(405);
   }
