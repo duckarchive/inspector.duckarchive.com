75dbef152fbcba85ba1668116130c96ea2e22270
alexandrtovmach <alexandrtovmach@gmail.com>
2024-06-24 16:23:29 +0200

    fix: Cleanup

--- app/archives/page.tsx
+++ app/archives/page.tsx
@@ -2,29 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { Archive } from "@prisma/client";
-import {
-  Box,
-  Card,
-  CardBody,
-  CardHeader,
-  Container,
-  HStack,
-  Heading,
-  IconButton,
-  Stat,
-  StatArrow,
-  StatHelpText,
-  StatLabel,
-  StatNumber,
-  Table,
-  Td,
-  Text,
-  Th,
-  Tr,
-  VStack,
-} from "@chakra-ui/react";
+import { Table, Td, Th, Tr } from "@chakra-ui/react";
 import { NextPage } from "next";
-import { IoArrowForward } from "react-icons/io5";
 import { Link } from "@chakra-ui/next-js";
 
 const ArchivesPage: NextPage = () => {
@@ -41,22 +20,24 @@ const ArchivesPage: NextPage = () => {
   }, []);
 
   return (
-      <Table bg="white">
-        <Tr key="archives-table-header" w="full">
-          <Th>Назва</Th>
-          <Th textAlign="right">Доступно онлайн</Th>
-          <Th textAlign="right">Остання зміна</Th>
+    <Table bg="white">
+      <Tr key="archives-table-header" w="full">
+        <Th>Назва</Th>
+        <Th textAlign="right">Доступно онлайн</Th>
+        <Th textAlign="right">Остання зміна</Th>
+      </Tr>
+      {archives.map((archive) => (
+        <Tr key={archive.id} w="full">
+          <Td>
+            <Link href={`archives/${archive.code}`} color="blue.600">
+              {archive.title}
+            </Link>
+          </Td>
+          <Td textAlign="right">566471</Td>
+          <Td textAlign="right">вчора</Td>
         </Tr>
-        {archives.map((archive) => (
-          <Tr key={archive.id} w="full">
-            <Td>
-              <Link href={`archives/${archive.code}`} color="blue.600">{archive.title}</Link>
-            </Td>
-            <Td textAlign="right">566471</Td>
-            <Td textAlign="right">вчора</Td>
-          </Tr>
-        ))}
-      </Table>
+      ))}
+    </Table>
   );
 };
 
