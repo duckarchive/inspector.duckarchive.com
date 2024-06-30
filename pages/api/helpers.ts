f7b187ae97a841260ee48023847e205df71c9a84
alexandrtovmach <alexandrtovmach@gmail.com>
2024-06-30 11:38:38 +0200

    feat: Added temp solution

--- pages/api/helpers.ts
+++ pages/api/helpers.ts
@@ -14,7 +14,13 @@ export const parseDBParams = (str: string | null): Record<string, string> => {
 export const parseCode = (str: string): string => {
   // input: "Р-34" "П-159" "Р34" "П159" "8дод." "2т.1" "10."
   // output: "Р34" "П159" "34" "159" "8дод" "2т1" "10"
-  return unescape(str).replace(/[^а-яА-Я0-9]/g, '');
+  const pure = unescape(str);
+  if (/\d+\./.test(pure)) {
+    // temporary solution for "10." in https://e-resource.tsdavo.gov.ua/fonds/8/
+    return pure.replace(/\./, 'н');
+  }
+
+  return pure.replace(/[^А-ЯҐЄІЇ0-9]/gi, '');
 }
 
 export const parseTitle = (str: string): string => {
