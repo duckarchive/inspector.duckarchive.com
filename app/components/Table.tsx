a5834801f23c32c27cea1ba2523d8bcf29a34dca
Alexandr Tovmach <alexandrtovmach@gmail.com>
2024-07-03 00:47:41 +0200

    fix: Debatable fix to remove h from table

--- app/components/Table.tsx
+++ app/components/Table.tsx
@@ -34,7 +34,7 @@ const DuckTable = <T extends { id: string }>({
   // }, [rows]);
 
   return (
-    <Box className="ag-theme-alpine" h="60vh" w="100%">
+    <Box className="ag-theme-alpine" w="100%">
       <AgGridReact
         ref={gridRef}
         domLayout="autoHeight"
