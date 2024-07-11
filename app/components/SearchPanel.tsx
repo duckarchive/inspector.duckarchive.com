d81f84b1cce5966e514fe2d1ec5a4cc214969b9e
alexandrtovmach <alexandrtovmach@gmail.com>
2024-07-11 17:05:22 +0200

    fix: Added search panel disclaimer

--- app/components/SearchPanel.tsx
+++ app/components/SearchPanel.tsx
@@ -1,13 +1,8 @@
-import {
-  Grid,
-  Input,
-  Stack,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
+import { Grid, Input, Stack, Text, VStack } from "@chakra-ui/react";
 import { ChangeEvent } from "react";
 import ArchiveSelect from "./Fields/ArchiveSelect";
 import { SearchRequest } from "../../pages/api/search";
+import { Link } from "@chakra-ui/next-js";
 
 interface SearchPanelProps {
   values?: SearchRequest;
@@ -17,7 +12,7 @@ interface SearchPanelProps {
 const SearchPanel: React.FC<SearchPanelProps> = ({ values, onChange }) => {
   const handleArchiveChange = (archiveCode?: string) => {
     onChange({ ...values, archiveCode });
-  }
+  };
 
   const handleFundChange = (e: ChangeEvent<HTMLInputElement>) => {
     onChange({ ...values, fundCode: e.target.value || undefined });
@@ -25,15 +20,21 @@ const SearchPanel: React.FC<SearchPanelProps> = ({ values, onChange }) => {
 
   const handleDescriptionChange = (e: ChangeEvent<HTMLInputElement>) => {
     onChange({ ...values, descriptionCode: e.target.value || undefined });
-  }
+  };
 
   const handleCaseChange = (e: ChangeEvent<HTMLInputElement>) => {
     onChange({ ...values, caseCode: e.target.value || undefined });
-  }
+  };
 
   return (
     <VStack minW="xs" maxW="md">
-      <Grid templateColumns='2fr repeat(3, 1fr)' gap={2} position="relative">
+      <Text fontSize="sm">
+        Введіть шифр справи, щоб знайти посилання на онлайн версію. Для перегляду архівів цілком, використовуйте{" "}
+        <Link href="/archives" color="blue.500">
+          сторінку архівів.
+        </Link>
+      </Text>
+      <Grid templateColumns="2fr repeat(3, 1fr)" gap={2} position="relative" mt={4}>
         <Stack gap={0}>
           <Text as="label" textTransform="uppercase" fontSize="xs" color="gray.500">
             архів
