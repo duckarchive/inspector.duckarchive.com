b3e13945c8b2e2f14e6d7505e2e97b2057aa8596
alexandrtovmach <alexandrtovmach@gmail.com>
2024-08-12 17:45:46 +0200

    feat: Added more loader texts; Wrapped search page with suspense

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,12 +1,16 @@
 import { NextPage } from "next";
 import { getArchives } from "@/data/archives";
 import Search from "@/components/search";
+import { Suspense } from "react";
+import Loader from "@/components/loader";
 
 const SearchPage: NextPage = async () => {
   const archives = await getArchives();
 
   return (
-    <Search archives={archives} />
+    <Suspense fallback={<Loader />}>
+      <Search archives={archives} />
+    </Suspense>
   );
 };
 
