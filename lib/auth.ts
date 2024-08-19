4562b041a354de5c1c5322374cd56dff9c6025cd
alexandrtovmach <alexandrtovmach@gmail.com>
2024-08-19 03:45:59 +0200

    fix: Disabled auth to test

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,10 +4,10 @@ export const isAuthorized = async (req: NextApiRequest) => {
   // check if request coming from duck-inspector.netlify.app
   // if not, return 403
   // log IP address
-  if (req.headers.origin !== "https://duck-inspector.netlify.app") {
-    console.log("isAuthorized: origin mismatch", req.headers.origin);
-    return false;
-  }
+  // if (req.headers.origin !== "https://duck-inspector.netlify.app") {
+  //   console.log("isAuthorized: origin mismatch", req.headers.origin);
+  //   return false;
+  // }
   return true;
 }
 
