15868401f16d7508f0389c05df890387fb040fd0
alexandrtovmach <alexandrtovmach@gmail.com>
2024-08-21 13:15:19 +0200

    fix: Made render conditional

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -8,10 +8,11 @@ import ReportModal from "@/components/report-modal";
 const ReportPage: NextPage = async () => {
   const resources = await getResources();
   const [report, reportSummary] = await getYesterdayReport();
+  const note = report.length >= 10000 ? " Максимальна кількість знахідок обмежена 10,000.": "";
 
   return (
     <>
-      <PagePanel title="Звіт" description="Список справ онлайн, знайдених за минулу добу. З ціллю оптимізації, максимальна кількість справ до відображення обмежено 10,000.">
+      <PagePanel title="Звіт" description={`Список справ онлайн, знайдених за минулу добу.${note}`}>
         <ReportModal data={reportSummary} />
       </PagePanel>
       <ReportTable resources={resources} report={report} />
