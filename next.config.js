4ac827dbd8e69dd91d49f599d7a1e57445e38a20
alexandrtovmach <alexandrtovmach@gmail.com>
2024-09-23 12:20:51 +0200

    chore: Changed cores count to use

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   staticPageGenerationTimeout: 240,
+  experimental: {
+    workerThreads: false,
+    cpus: 2,
+  },
 }
 
 module.exports = nextConfig
