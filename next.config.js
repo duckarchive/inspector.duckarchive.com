640d6fbe343300507e6ef9bd1d16e4d9e13e9fd4
alexandrtovmach <alexandrtovmach@gmail.com>
2024-09-24 19:32:04 +0200

    chore: Removed build limitations

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   staticPageGenerationTimeout: 240,
-  experimental: {
-    workerThreads: false,
-    cpus: 2,
-  },
 }
 
 module.exports = nextConfig
