366d7a7c6a7347cb99f5d707afc319816130f0f0
alexandrtovmach <alexandrtovmach@gmail.com>
2024-10-31 20:32:12 +0100

    feat: Updated list of allowed methods

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "https://www.familysearch.org" },
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST" },
           {
             key: "Access-Control-Allow-Headers",
             value:
