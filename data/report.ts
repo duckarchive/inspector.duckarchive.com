0223b7a3ebe79f10960074f9b2353aa85e7bea50
alexandrtovmach <alexandrtovmach@gmail.com>
2024-12-27 22:22:02 +0100

    feat: Updated report generation

--- data/report.ts
+++ data/report.ts
@@ -1,7 +1,7 @@
 import fs from "fs/promises";
 import groupBy from "lodash/groupBy.js";
 import prisma from "../lib/db";
-import { Archive, Case, Description, Fund, Match, MatchResult } from "@prisma/client";
+import { Archive, Case, Description, Fund, Match } from "@prisma/client";
 import chunk from "lodash/chunk.js";
 
 process.env.TZ = "UTC";
@@ -39,112 +39,58 @@ export const getYesterdayReport = async (): Promise<[Report, ReportSummary]> => 
   endOfYesterday.setHours(23, 59, 59, 999);
   const to = endOfYesterday.toISOString();
 
-  let prevMatchResults: MatchResult[] = await prisma.$queryRaw`
-    with
-      prev_match_results as (
-        select match_id, count, created_at, row_number() over (partition by match_id order by created_at desc) as rn
-        from match_results
-        where created_at < ${from}::timestamp and error is null
-      )
-    select
-      pmr.created_at,
-      pmr.match_id,
-      pmr.count
-    from prev_match_results pmr
-    where pmr.rn = 1;
-  `;
-
-  const prevMatchResultsHash: Record<MatchResult["match_id"], MatchResult["count"]> = {};
-
-  prevMatchResults.forEach((result) => {
-    prevMatchResultsHash[result.match_id] = result.count;
-  });
-
-  // garbage collection
-  prevMatchResults = null as any;
-
-  let lastMatchResults: MatchResult[] = await prisma.$queryRaw`
-    with
-      last_match_results as (
-        select match_id, count, created_at, row_number() over (partition by match_id order by created_at desc) as rn
-        from match_results
-        where created_at >= ${from}::timestamp and created_at < ${to}::timestamp and error is null
-      )
-    select 
-      lmr.created_at,
-      lmr.match_id,
-      lmr.count
-    from last_match_results lmr
-    where lmr.rn = 1;
-  `;
-
-  const updatedMatchResultsHash: Record<MatchResult["match_id"], boolean> = {};
-
-  const updatedMatchResults = lastMatchResults.filter((result) => {
-    const prevCount = prevMatchResultsHash[result.match_id] || 0;
-    if (prevCount !== result.count) {
-      updatedMatchResultsHash[result.match_id] = result.count > prevCount;
-      return true;
-    }
-  });
-
-  // garbage collection
-  lastMatchResults = null as any;
-
-  const updatedMatchResultIds = updatedMatchResults.map((result) => result.match_id);
-  const updatedMatchResultIdsChunks = chunk(updatedMatchResultIds, 1000);
-
-  let updatedMatches: any[] = [];
-  for (const updatedMatchResultIdsChunk of updatedMatchResultIdsChunks) {
-    const dbMatches = await prisma.match.findMany({
-      where: {
-        id: {
-          in: updatedMatchResultIdsChunk,
+  const updatedMatches = await prisma.match.findMany({
+    where: {
+      updated_at: {
+        gte: from,
+        lt: to,
+      },
+      children_count: {
+        not: {
+          equals: prisma.match.fields.prev_children_count
+        }
+      }
+    },
+    select: {
+      id: true,
+      updated_at: true,
+      resource_id: true,
+      archive: {
+        select: {
+          code: true,
         },
       },
-      select: {
-        id: true,
-        updated_at: true,
-        resource_id: true,
-        archive: {
-          select: {
-            code: true,
-          },
+      fund: {
+        select: {
+          code: true,
         },
-        fund: {
-          select: {
-            code: true,
-          },
-        },
-        description: {
-          select: {
-            code: true,
-          },
-        },
-        case: {
-          select: {
-            code: true,
-          },
-        },
-        children_count: true,
-        url: true,
       },
-    });
+      description: {
+        select: {
+          code: true,
+        },
+      },
+      case: {
+        select: {
+          code: true,
+        },
+      },
+      children_count: true,
+      url: true,
+    },
+  });
 
-    updatedMatches = updatedMatches.concat(dbMatches);
-  }
-
-  const report = updatedMatches.map((match) => ({
+  const report: Report = updatedMatches.map((match) => ({
     id: match.id,
     updated_at: match.updated_at,
     resource_id: match.resource_id,
-    archive_code: match.archive.code,
-    fund_code: match.fund.code,
-    description_code: match.description.code,
-    case_code: match.case.code,
+    archive_code: match.archive?.code || "unknown",
+    fund_code: match.fund?.code || "unknown",
+    description_code: match.description?.code || "unknown",
+    case_code: match.case?.code || "unknown",
     children_count: match.children_count,
     url: match.url,
-    is_online: updatedMatchResultsHash[match.id],
+    is_online: Boolean(match?.children_count),
   }));
 
   // const groupedByArchive = Object.entries(groupBy(report, "archive_code")).map(([code, rows]) => ({
