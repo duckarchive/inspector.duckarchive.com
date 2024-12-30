c603001b95ab793f1b1903d2898c1c266d4d8b6e
alexandrtovmach <alexandrtovmach@gmail.com>
2024-12-30 22:13:08 +0100

    feat: Added PATCH method to allowed CORS rules

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const corsHeaders = [
   { key: "Access-Control-Allow-Credentials", value: "true" },
   { key: "Access-Control-Allow-Origin", value: "*" },
-  { key: "Access-Control-Allow-Methods", value: "GET,POST" },
+  { key: "Access-Control-Allow-Methods", value: "GET,POST,PATCH" },
   {
     key: "Access-Control-Allow-Headers",
     value:
