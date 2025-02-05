734e6e5e6a403d8546d38160c7c20504d3d06f07
alexandrtovmach <alexandrtovmach@gmail.com>
2025-02-05 13:13:33 +0100

    feat: Added error and draft not-found handlers

--- app/_not-found.tsx
+++ app/_not-found.tsx
@@ -1,24 +1,13 @@
-"use client";
-
-import { useEffect } from "react";
 import ErrorDuckSrc from "@/public/images/error-duck.png";
 import Image from "next/image";
 import { Link } from "@heroui/link";
+import { NextPage } from "next";
 
-interface ErrorComponentProps {
-  error: Error;
-  reset: () => void;
-}
-
-const ErrorComponent: React.FC<ErrorComponentProps> = ({ error }) => {
-  useEffect(() => {
-    console.error(error);
-  }, [error]);
-
+const NotFound: NextPage = () => {
   return (
     <div className="w-80 m-auto flex items-center flex-col">
       <p className="text-lg text-center">
-        Щось зламалось. Спробуйте перезавантажити сторінку або напишіть:&nbsp;
+        Сторінка не знайдена
         <Link href="mailto:admin@duckarchive.com" target="_blank" className="text-lg">admin@duckarchive.com</Link>
       </p>
       <Image src={ErrorDuckSrc} width={300} height={300} alt="error duck" />
@@ -27,4 +16,4 @@ const ErrorComponent: React.FC<ErrorComponentProps> = ({ error }) => {
   );
 };
 
-export default ErrorComponent;
+export default NotFound;
