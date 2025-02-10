0fe4f9b99a54e92789a1ecfe94a2da37a9aab303
alexandrtovmach <alexandrtovmach@gmail.com>
2025-02-10 14:59:50 +0100

    feat: Updated icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,6 @@ export const metadata: Metadata = {
     template: `%s | ${siteConfig.name}`,
   },
   description: siteConfig.description,
-  icons: {
-    icon: "/favicon.ico",
-  },
 };
 
 export const viewport: Viewport = {
