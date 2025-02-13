03fd0a04c910637c6b196ea250ef3aad565b99a4
alexandrtovmach <alexandrtovmach@gmail.com>
2025-02-13 22:07:57 +0100

    feat: Disabled Yandexbot

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,19 @@
-import type { MetadataRoute } from 'next'
-import { siteConfig } from '@/config/site'
- 
+import type { MetadataRoute } from "next";
+import { siteConfig } from "@/config/site";
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/admin/',
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: "/admin/",
+      },
+      {
+        userAgent: "Yandex",
+        disallow: "/",
+      },
+    ],
     sitemap: `${siteConfig.url}/sitemap.xml`,
-  }
+  };
 }
