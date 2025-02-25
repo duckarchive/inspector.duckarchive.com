b5da0a55ccc6b3364f3cef80a28f894230038ed9
alexandrtovmach <alexandrtovmach@gmail.com>
2025-02-25 22:01:26 +0100

    feat: Changed loading skeleton

--- app/search/loading.tsx
+++ app/search/loading.tsx
@@ -1,17 +1,24 @@
 "use client";
 
 import { Skeleton } from "@heroui/react";
+import DuckTableSkeleton from "@/components/duck-table-skeleton";
 
 export default function Loading() {
   return (
-    <div className="flex-col md:flex-row flex justify-between gap-4">
-      <div className="flex flex-col grow items-start gap-2 p-2">
-        <Skeleton className="rounded-lg h-5 w-1/3" />
-        <Skeleton className="rounded-lg h-4 w-full" />
-        <Skeleton className="rounded-lg h-4 w-full" />
-        <Skeleton className="rounded-lg h-4 w-2/3" />
+    <div className="flex-col flex justify-between gap-2">
+      <Skeleton className="rounded-lg h-5 w-1/3" />
+      <Skeleton className="rounded-lg h-14 w-full" />
+      <div className="flex flex-wrap gap-2">
+        <Skeleton className="rounded-lg h-14 grow" />
+        <Skeleton className="rounded-lg h-14 grow" />
+        <Skeleton className="rounded-lg h-14 grow" />
       </div>
-      <Skeleton className="rounded-lg w-full md:w-1/2 h-[200px]" />
+      <Skeleton className="rounded-lg h-14 w-full" />
+      <div className="flex flex-wrap justify-between w-full">
+        <Skeleton className="rounded-lg h-4 w-1/3" />
+        <Skeleton className="rounded-lg h-4 w-1/5" />
+      </div>
+      <DuckTableSkeleton />
     </div>
   );
 }
