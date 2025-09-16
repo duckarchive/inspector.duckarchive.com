f63c6ea9f8f271aa4b3864fe70833d77d10f8366
alexandrtovmach <alexandrtovmach@gmail.com>
2025-09-16 17:08:45 +0200

    fix: Replaced dev artifact

--- config/site.ts
+++ config/site.ts
@@ -2,6 +2,6 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "title",
-  url: "http://localhost:3000",
+  url: "https://inspector.duckarchive.com",
   description: "description",
 };
