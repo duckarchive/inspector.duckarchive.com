7f876878e9127a6f205fa1ead0393ec6457b3d6b
Alexandr Tovmach <alexandrtovmach@gmail.com>
2025-11-11 13:54:13 +0100

    fix: Revert dev artifacts

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,6 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "title",
-  url: "http://localhost:3000",
-  // url: "https://inspector.duckarchive.com",
+  url: "https://inspector.duckarchive.com",
   description: "description",
 };
