d36672c5b5d5e9d09a5676a7f15d052d704de69e
Alexandr Tovmach <alexandrtovmach@gmail.com>
2026-05-31 02:18:20 +0200

    feat: Updated 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,14 @@
-import NotFoundDuckSrc from "@/public/images/not-found-duck.png";
+import NotFoundSrc from "@/public/images/not-found.png";
 import Image from "next/image";
 import { NextPage } from "next";
+import { getTranslations } from "next-intl/server";
 
-const NotFound: NextPage = () => {
+const NotFound: NextPage = async () => {
+  const translations = await getTranslations();
   return (
-    <div className="w-80 m-auto flex items-center flex-col">
-      <p className="text-lg text-center">
-        Сторінка не знайдена
-      </p>
-      <Image src={NotFoundDuckSrc} width={300} height={300} alt="404 duck" />
+    <div className="h-screen m-auto flex items-center justify-center flex-col">
+      <p className="text-lg text-center">{translations("not-found.title")}</p>
+      <Image src={NotFoundSrc} width={500} height={500} className="absolute bottom-0 max-w-full" alt="404 duck" />
     </div>
   );
 };
